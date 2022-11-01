71aa2044487975c7d1005f94d75569de6001521e
joku <jokumusic@gmail.com>
2022-10-31 17:06:44 -0700

    Game passed to screen-tictactoe-game and grid successfully displayed

--- src/screen-tictactoe-game.tsx
+++ src/screen-tictactoe-game.tsx
@@ -2,24 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Text, useNavigation, View, Image} from "react-xnft";
 
 
-
-
 const viewHeight = 500;
-const rows = 6;
-const cols = 6;
-const cellsize = 1/rows * viewHeight;
 const xImgUri = "https://media.istockphoto.com/vectors/dirty-grunge-hand-drawn-with-brush-strokes-cross-x-vector-isolated-vector-id1201202836?k=20&m=1201202836&s=612x612&w=0&h=0ib7KQaJXonfKN0-tGNaMUIw2Hre9sJjd4hTsc3QwBc=";
 const oImgUri = "https://us.123rf.com/450wm/rondale/rondale1701/rondale170100555/69948558-fire-letter-o-of-burning-blue-flame-flaming-burn-font-or-bonfire-alphabet-text-with-sizzling-smoke-a.jpg?ver=6";
 
+const mockGame = {rows:5,cols:5};
+
 export function ScreenTicTacToeGame() {
   const nav = useNavigation();
-  const game = useState(nav.activeRoute.props.game);
+  const [game] = useState(nav.activeRoute.props?.game);
+  const [cellsize] = useState(1/game.rows * viewHeight);
   const [playerTurn, setPlayerTurn] = useState(0);
-  const [matrix, setMatrix] = useState(Array.from({length: rows},()=> Array.from({length: cols}, () => null)));
+  const [matrix, setMatrix] = useState(Array.from({length: game.rows},()=> Array.from({length: game.cols}, () => null)));
 
-  useEffect(()=>{
-    console.log('ttt gamescreen: ', game);
-  },[]);
 
   function getCellImage(row,col) {
     const state = matrix[row][col];
@@ -56,7 +51,7 @@ export function ScreenTicTacToeGame() {
     return (<View style={{display: 'flex', flexDirection: 'row', width: '100%'}}>{elements}</View>);
   }
   
-  function getTable(rows: number, cols: number ) {
+  function getTable(rows: number, cols: number) {
     const elements = [];
     for(let row=0;row<rows; row++)
       elements.push(getRow(cols,row));
@@ -64,9 +59,15 @@ export function ScreenTicTacToeGame() {
     return (<View style={{display: 'flex', flexDirection: 'column', alignContent: 'center', alignItems: 'center'}}>{elements}</View>);
   }
 
+  async function refreshGrid() {
+
+  }
+
   return (
     <View>
-      {getTable(rows,cols)}
+      { 
+          getTable(game.rows,game.cols)
+      }
     </View>
   );
 }
